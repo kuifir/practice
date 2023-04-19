83990ed0b51db5dd3db8d0059a15719fc1b29e4d
kuifir <kuifir@163.com>
2023-04-19 22:10:27 +0800

    feat:java-up: 异常处理增加异常信息输出

--- java-up/src/main/java/com/kuifir/exception/resolve/union/Digest.java
+++ java-up/src/main/java/com/kuifir/exception/resolve/union/Digest.java
@@ -7,7 +7,14 @@ public sealed abstract class Digest {
         return switch (algorithm) {
             case "SHA-256" -> new Returned.ReturnValue(new SHA256());
             case "SHA-512" -> new Returned.ReturnValue(new SHA512());
-            case null, default -> new Returned.ErrorCode(-1);
+            case null -> {
+                System.getLogger("com.kuifir.exception.resolve.union").log(System.Logger.Level.WARNING, "No algorithm is specified", new Throwable("the calling stack"));
+                yield new Returned.ErrorCode(-1);
+            }
+            default -> {
+                System.getLogger("com.kuifir.exception.resolve.union").log(System.Logger.Level.INFO, "Unknown algorithm is specified " + algorithm, new Throwable("the calling stack"));
+                yield new Returned.ErrorCode(-1);
+            }
         };
     }
 
--- java-up/src/main/java/com/kuifir/exception/resolve/union/UseCase.java
+++ java-up/src/main/java/com/kuifir/exception/resolve/union/UseCase.java
@@ -3,13 +3,16 @@ package com.kuifir.exception.resolve.union;
 public class UseCase {
     public static void main(String[] args) {
 
-        Returned<Digest> rt = Digest.of("SHA-256");
+
+        Returned<Digest> rt = Digest.of("SHA-128");
         switch (rt) {
             case Returned.ReturnValue rv -> {
                 Digest d = (Digest) rv.returnValue();
                 d.digest("Hello, world!".getBytes());
             }
-            case Returned.ErrorCode ignored -> System.out.println("Failed to get instance of SHA-256");
+            case Returned.ErrorCode ignored -> System.getLogger("com.kuifir.exception.resolve.union")
+                    .log(System.Logger.Level.INFO,
+                            "Failed to get instance of SHA-128");
         }
     }
 }
