a37bac57d76e65aee768430d82c3d4c0bd2cdbef
kuifir <kuifir@163.com>
2023-05-10 23:53:48 +0800

    用一个递归的lambda表达式来实现斐波那契函数，使用的实例变量，用构造器来初始化

---
+++ java-up/src/main/java/com/kuifir/normal/funtional/IntCall.java
@@ -0,0 +1,10 @@
+package com.kuifir.normal.funtional;
+
+/**
+ * @author kuifir
+ * @date 2023/5/10 23:44
+ */
+public interface IntCall {
+    /**/
+    int call(int arg);
+}
---
+++ java-up/src/main/java/com/kuifir/normal/funtional/RecursiveFibonacci.java
@@ -0,0 +1,27 @@
+package com.kuifir.normal.funtional;
+
+/**
+ * 用一个递归的lambda表达式来实现斐波那契函数，使用的实例变量，用构造器来初始化
+ * @author kuifir
+ * @date 2023/5/10 23:45
+ */
+public class RecursiveFibonacci {
+    IntCall fib;
+
+    public RecursiveFibonacci() {
+        this.fib = n -> n == 0 ? 0 :
+                n == 1 ? 1 :
+                        fib.call(n - 1) + fib.call(n - 2);
+    }
+
+    int fibonacci(int n) {
+        return fib.call(n);
+    }
+
+    public static void main(String[] args) {
+        RecursiveFibonacci recursiveFibonacci = new RecursiveFibonacci();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(recursiveFibonacci.fibonacci(i));
+        }
+    }
+}
