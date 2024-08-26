dfc3aed483759dd0793ffec0624e01e08bb34fa5
kuifir <kuifir@163.com>
2024-08-26 23:30:39 +0800

    feat:堆排序

--- data-structure/src/main/java/com/kuifir/algorithm/sorting/selection/HeapSort.java
+++ data-structure/src/main/java/com/kuifir/algorithm/sorting/selection/HeapSort.java
@@ -12,15 +12,15 @@ public class HeapSort {
     public static void heapSort(int[] l) {
         // 构造大顶堆
         for (int i = l.length / 2; i > 0; i--) {
-            heapAdjust(l, i, l.length);
+            heapAdjust2(l, i, l.length);
         }
         System.out.println(Arrays.toString(l));
         // 筛选下一个最大值
         for (int i = 1; i < l.length; i++) {
             int tmp = l[0];
             l[0] = l[l.length - i];
-            l[l.length-i] = tmp;
-            heapAdjust(l, 1, l.length - i);
+            l[l.length - i] = tmp;
+            heapAdjust2(l, 1, l.length - i);
             System.out.println(Arrays.toString(l));
         }
     }
@@ -33,6 +33,23 @@ public class HeapSort {
                 int tmp = l[j / 2 - 1];
                 l[j / 2 - 1] = l[max];
                 l[max] = tmp;
+            } else {
+                break;
+            }
+        }
+    }
+
+    // m+1 至 n 符合大顶堆，将m 至n 调整到最大堆
+    public static void heapAdjust2(int[] l, int m, int n) {
+        while (2 * m <= n) {
+            int left = 2 * m;
+            int right = 2 * m + 1;
+            int max = (left < n) && (l[right - 1] > l[left - 1]) ? right : left;
+            if (l[m - 1] < l[max - 1]) {
+                int tmp = l[m - 1];
+                l[m - 1] = l[max - 1];
+                l[max - 1] = tmp;
+                m = max;
             } else {
                 break;
             }
